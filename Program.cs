02a0bcf33b198420af6a6bae02eaabebaf34423b
Igor Yalovoy <igor@yalovoi.net>
2015-09-01 17:13:29 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using Flurl;
+using Flurl.Http;
+using Newtonsoft.Json;
+
+namespace demibyte.coinbase
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var host = "https://api.coinbase.com/";
+			var apiKey = "yourApiKey";
+			var apiSecret = "youApiSecret";
+
+			var unixTimestamp = (Int32)(DateTime.UtcNow.Subtract (new DateTime (1970, 1, 1))).TotalSeconds;
+			var currency = "USD";
+			var message = string.Format ("{0}GET/v2/prices/spot?currency={1}", unixTimestamp.ToString (), currency);
+
+			byte[] secretKey = Encoding.UTF8.GetBytes (apiSecret);
+			HMACSHA256 hmac = new HMACSHA256 (secretKey);
+			hmac.Initialize ();
+			byte[] bytes = Encoding.UTF8.GetBytes (message);
+			byte[] rawHmac = hmac.ComputeHash (bytes);
+			var signature = rawHmac.ByteArrayToHexString ();
+
+			var price = host
+				.AppendPathSegment ("v2/prices/spot")
+				.SetQueryParam ("currency", currency)
+				.WithHeader ("CB-ACCESS-SIGN", signature)
+				.WithHeader ("CB-ACCESS-TIMESTAMP", unixTimestamp)
+				.WithHeader ("CB-ACCESS-KEY", apiKey)
+				.GetJsonAsync<dynamic> ()
+				.Result;
+
+			Console.WriteLine (price.ToString (Formatting.None));
+			Console.ReadLine ();
+		}
+	}
+}
---
+++ StringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace demibyte.coinbase
+{
+	public static class StringHelper
+	{
+		public static byte[] HexStringToByteArray (this string hex)
+		{
+			return Enumerable.Range (0, hex.Length)
+				.Where (x => x % 2 == 0)
+				.Select (x => Convert.ToByte (hex.Substring (x, 2), 16))
+				.ToArray ();
+		}
+
+		public static string ByteArrayToHexString (this byte[] array)
+		{
+			StringBuilder hex = new StringBuilder (array.Length * 2);
+			foreach (byte b in array)
+				hex.AppendFormat ("{0:x2}", b);
+			return hex.ToString ();
+		}
+	}
+}
